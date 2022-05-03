4472a1e7be1ff883fd4c3882e216f508efb37330
rknnakamura <rkn.nakamura@gmail.com>
2022-05-02 21:45:28 -0300

    Criação APIs testes

---
+++ RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person? GetByID(long id);
+        List<Person> GetAll();
+        Person? Update(Person person);
+        void delete(long id);
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,47 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+
+        private List<Person> _people = new List<Person>();
+
+        public Person Create(Person person)
+        {
+            _people.Add(person);
+            return person;
+        }
+
+        public void delete(long id)
+        {
+            var person = GetByID(id);
+            if (person != null ) _people.Remove(person);
+        }
+
+        public List<Person> GetAll()
+        {
+            return _people;
+        }
+
+        public Person? GetByID(long id)
+        {
+            return _people.Find(x => x.Id == id);
+        }
+
+        public Person? Update(Person person)
+        {
+            var value = GetByID(person.Id);
+            if (value != null)
+            {
+                value.FirstName = person.FirstName;
+                value.LastName = person.LastName;
+                value.Address = person.Address;
+                value.Gender = person.Gender;
+            }
+
+            return value;
+
+        }
+    }
+}
